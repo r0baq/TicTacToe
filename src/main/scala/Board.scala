90f55be140e0c81cab27654e146845821f4e8ace
Robert Mroczka <mroczka.robert@gmail.com>
2018-09-06 19:01:15 +0200

    Zakończenie wersji konsolowej.

--- src/main/scala/Board.scala
+++ src/main/scala/Board.scala
@@ -1,6 +1,6 @@
 object Board {
 
-  private val data = Array.ofDim[Char](3, 3)
+  private val data = Array.ofDim[Player.Value](3, 3)
   private var currentPlayer = Player.O
 
   def printBoard() = {
@@ -17,20 +17,19 @@ object Board {
 
   def add(read: String) = {
     val numbers = read.split(" ")
-    data(numbers(0).toInt)(numbers(1).toInt) = currentPlayer.toString.charAt(0)
+    data(numbers(0).toInt)(numbers(1).toInt) = currentPlayer
     changePlayer()
   }
 
   private def changePlayer() = if (currentPlayer == Player.O) currentPlayer = Player.X else currentPlayer = Player.O
 
-  private def printRow(row : Array[Char]): Unit = {
-    println(row(0) + "|" + row(1) + "|" + row(2))
-  }
+  private def printRow(row : Array[Player.Value]) = println(printCell(row(0)) + "|" + printCell(row(1)) + "|" + printCell(row(2)))
 
-  private def isWon(player : Player.Value) = {
+  private def printCell(cell: Player.Value) = if (cell == null) " " else cell
+
+  private def isWon(player : Player.Value) =
     horizontal(player, 0) || horizontal(player, 1) || horizontal(player, 2) || vertical(player, 0)|| vertical(player, 1) || vertical(player, 2) ||
       diagonalFirst(player) || diagonalSecond(player)
-  }
 
   private def horizontal(player: Player.Value, row: Int) =
     data(row)(0) == player && data(row)(1) == player && data(row)(2) == player
